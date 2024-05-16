e09757637e79e38b7c88ba8d9a7a7c5fa5f0f0df
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-05-16 12:48:32 -0700

    Added ToDoItemModel

---
+++ app/src/main/java/com/example/todolist/ToDoItemModel.java
@@ -0,0 +1,9 @@
+package com.example.todolist;
+
+public class ToDoItemModel {
+    //id: The id number of each task
+    //status: The status of each task (Boolean value(0=true, 1=false))
+    //task: The task itself typed in by user
+    private int id, status;
+    private String task;
+}
